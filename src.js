886952e1dd1f3283d7f4f16a222da9d723a78401
Camilo Riobamba <camilo.riobamba@gmail.com>
2021-11-29 20:03:28 -0500

    feat: registerCO

--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-const calcularPorcentajes = (limiteCO, limiteCO2, limiteHC, limiteO2,
+module.exports.calcularPorcentajes = (limiteCO, limiteCO2, limiteHC, limiteO2,
     valorCO, valorCO2, valorHC, valorO2) => {
 
     let porcentajeCO = limiteCO ? valorCO * 100 / limiteCO : 0;
@@ -6,7 +6,7 @@ const calcularPorcentajes = (limiteCO, limiteCO2, limiteHC, limiteO2,
     let porcentajeHC = limiteHC ? valorHC * 100 / limiteHC : 0;
     let porcentajeO2 = limiteO2 ? valorO2 * 100 / limiteO2 : 0;
 
-    calcularPorcentajes = {
+    return {
         "porcentajeCO ": porcentajeCO + "%",
         "porcentajeCO2": porcentajeCO2 + "%",
         "porcentajeHC ": porcentajeHC + "%",
@@ -14,15 +14,17 @@ const calcularPorcentajes = (limiteCO, limiteCO2, limiteHC, limiteO2,
     }
 }
 
-console.log(calcularPorcentajes)
-
-const registarCO = (value) => {
-
-
+module.exports.registarCO = (value) => {
+    if(value >= global.rangosCO[0].de && value <= global.rangosCO[0].hasta){
+        return global.rangosCO[0].etiqueta;
+    } else if(value >= global.rangosCO[1].de && value <= global.rangosCO[1].hasta){
+        return global.rangosCO[1].etiqueta;
+    } else{
+        return "fuera_de_rango";
+    }
 }
 
 module.exports.registrarCO = registarCO;
-module.exports.calcularPorcentajes = calcularPorcentajes;
 
 const registarCO2 = (value) => {
 
@@ -51,24 +53,23 @@ const registarO2 = (value) => {
 module.exports.registrarO2 = registrarO2;
 module.exports.calcularPorcentajes = calcularPorcentajes;
 
-/*
+
 global.rangosCO = [
-    { etriqueta: 'Parametro CO en rango estandar', de: 0, hasta: 10 },
-    { etriqueta: 'Parametro CO fuera de rango', de: 11, hasta: 15 },
+    { etiqueta: 'Parametro CO en rango estandar', de: 0, hasta: 10 },
+    { etiqueta: 'Parametro CO fuera de rango', de: 11, hasta: 15 },
 ]
 
 global.rangosCO2 = [
-    { etriqueta: 'Parametro CO2 en rango estandar', de: 0, hasta: 20 },
-    { etriqueta: 'Parametro CO2 fuera de rango', de: 21, hasta: 30 },
+    { etiqueta: 'Parametro CO2 en rango estandar', de: 0, hasta: 20 },
+    { etiqueta: 'Parametro CO2 fuera de rango', de: 21, hasta: 30 },
 ]
 
 global.rangosHC = [
-    { etriqueta: 'Parametro HC en rango estandar', de: 0, hasta: 1000 },
-    { etriqueta: 'Parametro HC fuera de rango', de: 10001, hasta: 11000 },
+    { etiqueta: 'Parametro HC en rango estandar', de: 0, hasta: 1000 },
+    { etiqueta: 'Parametro HC fuera de rango', de: 10001, hasta: 11000 },
 ]
 
 global.rangosO2 = [
-    { etriqueta: 'Parametro O2 en rango estandar', de: 0, hasta: 22 },
-    { etriqueta: 'Parametro O2 fuera de rango', de: 13, hasta: 30 },
+    { etiqueta: 'Parametro O2 en rango estandar', de: 0, hasta: 22 },
+    { etiqueta: 'Parametro O2 fuera de rango', de: 13, hasta: 30 },
 ]
-*/
